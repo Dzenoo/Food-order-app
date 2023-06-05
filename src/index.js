c33b4a8956495e738b59303944635b6008dea2e5
Dzenis Gudzevic <frontprogramer@gmail.com>
2023-06-05 22:30:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import store from "./store/index";
 import { Provider } from "react-redux";
